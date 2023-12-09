687667147d624b45d579e5016bab197a29c1f61f
Hgioti <60378883+Hgioti@users.noreply.github.com>
2023-12-09 18:09:44 +0200

    Add files via upload

--- css.js
+++ css.js
@@ -37,7 +37,7 @@ body {
 
 
 
-@media only sceen and (min-width:801px) {
+@media only sceen and (min-width:481px) {
   /* For desktop: */
 
     .margin-resume {
@@ -75,7 +75,7 @@ body {
 
 
 
-@media only sceen and (max-width:800px){
+@media only sceen and (max-width:480px){
   /* For mobile phones: */
 
   .margin-resume{
